7f7a046110ff6dba413eb93bef42d1492981e85c
Hyeonkkyu <101560173+Hyeonkkyu@users.noreply.github.com>
2022-09-27 17:48:58 +0900

    22.09.27

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Main from './pages/Main';
+import Header from './components/Header';
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+
+export default App
